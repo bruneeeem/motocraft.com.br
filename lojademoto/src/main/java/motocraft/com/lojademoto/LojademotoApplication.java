5b8e81905a746abb9146e1e15158b91f6de85c92
bruno.malmeida3 <bruno.malmeida3@CAS554773W10F05.senacsp.edu.br>
2019-05-09 20:05:54 -0300

    O Inicio de tudo...

---
+++ lojademoto/src/main/java/motocraft/com/lojademoto/LojademotoApplication.java
@@ -0,0 +1,13 @@
+package motocraft.com.lojademoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojademotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojademotoApplication.class, args);
+	}
+
+}
