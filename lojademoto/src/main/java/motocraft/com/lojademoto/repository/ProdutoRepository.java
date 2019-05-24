52fbfd390919bef00c39903a0e54a6dc70449fd1
rbezerra <rbezerra@SAO-4WVXFM2.usersad.everis.int>
2019-05-23 21:09:11 -0300

    Adicionando Transação de Salvar venda

--- lojademoto/src/main/java/motocraft/com/lojademoto/repository/ProdutoRepository.java
+++ lojademoto/src/main/java/motocraft/com/lojademoto/repository/ProdutoRepository.java
@@ -9,7 +9,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 import motocraft.com.lojademoto.entidade.Produto;
+import motocraft.com.lojademoto.entidade.Venda;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -29,5 +31,13 @@ public class ProdutoRepository {
                         .setMaxResults(quantidade);
         return jpqlQuery.getResultList();
     }
+    
+    @Transactional
+    public void save(Venda v) {
+            // Salva um novo produto
+            entityManager.persist(v);
+        }
+    
 
 }
+
