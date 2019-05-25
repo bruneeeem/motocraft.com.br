56d3ddd194aba1bf8cd4beb2574215da77386e0b
Daniel <danielfsantos56@gmail.com>
2019-05-25 20:48:41 -0300

    Tela de produtos carregando produtos

--- lojademoto/src/main/java/motocraft/com/lojademoto/controller/ProdutoController.java
+++ lojademoto/src/main/java/motocraft/com/lojademoto/controller/ProdutoController.java
@@ -7,18 +7,39 @@ package motocraft.com.lojademoto.controller;
 
 import java.util.List;
 import motocraft.com.lojademoto.entidade.Produto;
+import motocraft.com.lojademoto.repository.ClienteRepository;
+import motocraft.com.lojademoto.repository.ItensPedidoRepository;
+import motocraft.com.lojademoto.repository.PedidoRepository;
 import motocraft.com.lojademoto.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/produto")
 public class ProdutoController {
+     
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    private ClienteRepository clienteRepository;
+    private PedidoRepository pedidoRepository;
+    private ItensPedidoRepository ipRepository;
+
+    @GetMapping
+    public ModelAndView listar(
+            @RequestParam(name = "offset", defaultValue = "0") int offset,
+            @RequestParam(name = "qtd", defaultValue = "100") int qtd){
+            //@RequestParam(name = "idsCat", required = false) List<Integer> idsCat) 
+        List<Produto> produtos;
+        produtos = produtoRepository.findAll(offset, qtd);
+        return new ModelAndView("product").addObject("produtos", produtos);
+    }
     
-    @GetMapping("/produto")
+    @GetMapping("/producto")
     public String product() {
         return "product";
     }
